32ff38a39ae6a2574c01f08a6c013ede31ef8248
axmf <axmf55@gmail.com>
2022-11-11 20:55:54 +0100

    Initialized project

---
+++ marketprocessor/src/test/java/com/marketprocessor/marketprocessor/MarketprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketprocessor.marketprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
