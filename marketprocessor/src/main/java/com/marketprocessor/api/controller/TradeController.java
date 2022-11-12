e2c9b3f3efe0b7a9b787f4c2183533d75e6479c7
axmf <axmf55@gmail.com>
2022-11-12 15:37:28 +0100

    Calling TradeController

--- marketprocessor/src/main/java/com/marketprocessor/api/controller/TradeController.java
+++ marketprocessor/src/main/java/com/marketprocessor/api/controller/TradeController.java
@@ -1,6 +1,8 @@
 package com.marketprocessor.api.controller;
 
 import com.marketprocessor.api.controller.model.Trade;
+import com.marketprocessor.processor.TradeProcessor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,14 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class TradeController {
 
+    private final TradeProcessor tradeProcessor;
+
     @PostMapping(value = "/trade")
     public void postMethodName(@RequestBody Trade trade) {
-        //TODO: process POST request
-        log.info("Trade received: {}", trade);
+        log.debug("Trade received: {}", trade);
+        tradeProcessor.processTrade(trade);
     }
-
-
 }
