5e32ec74ceae0a32f5255fed6e620f3b32669350
axmf <axmf55@gmail.com>
2022-11-12 19:39:24 +0100

    Converted the API into a kafka producer

--- marketprocessor/src/main/java/com/marketprocessor/processor/TradeProcessor.java
+++ marketprocessor/src/main/java/com/marketprocessor/processor/TradeProcessor.java
@@ -1,6 +1,8 @@
 package com.marketprocessor.processor;
 
 import com.marketprocessor.api.controller.model.Trade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -8,13 +10,17 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class TradeProcessor {
 
     private final Map<UUID, Trade> trades = new HashMap<>();
 
+    private final KafkaTemplate<Object, Object> kafkaTemplate;
+
     public void processTrade(Trade trade) {
         //TODO add a DB
         trades.put(UUID.randomUUID(), trade);
+        kafkaTemplate.send("trades", trade.toString());
     }
 
     public HashMap<UUID, Trade> getTrades() {
