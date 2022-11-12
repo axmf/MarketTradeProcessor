b760c5b01667ab61ec3e4b8d29587f3060ec0810
axmf <axmf55@gmail.com>
2022-11-12 15:42:26 +0100

    Added new endpoint to display stored trades

--- marketprocessor/src/main/java/com/marketprocessor/api/controller/TradeController.java
+++ marketprocessor/src/main/java/com/marketprocessor/api/controller/TradeController.java
@@ -4,9 +4,13 @@ import com.marketprocessor.api.controller.model.Trade;
 import com.marketprocessor.processor.TradeProcessor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 
 @RestController
@@ -17,8 +21,13 @@ public class TradeController {
     private final TradeProcessor tradeProcessor;
 
     @PostMapping(value = "/trade")
-    public void postMethodName(@RequestBody Trade trade) {
+    public void newTrade(@RequestBody Trade trade) {
         log.debug("Trade received: {}", trade);
         tradeProcessor.processTrade(trade);
+    }
+
+    @GetMapping(value = "/trade")
+    public HashMap<UUID, Trade> trades() {
+        return tradeProcessor.getTrades();
     }
 }
--- marketprocessor/src/main/java/com/marketprocessor/processor/TradeProcessor.java
+++ marketprocessor/src/main/java/com/marketprocessor/processor/TradeProcessor.java
@@ -3,17 +3,22 @@ package com.marketprocessor.processor;
 import com.marketprocessor.api.controller.model.Trade;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class TradeProcessor {
 
-    private Map<UUID, Trade> trades;
+    private final Map<UUID, Trade> trades = new HashMap<>();
 
     public void processTrade(Trade trade) {
         //TODO add a DB
         trades.put(UUID.randomUUID(), trade);
+    }
+
+    public HashMap<UUID, Trade> getTrades() {
+        return new HashMap<>(trades);
     }
 
 }
