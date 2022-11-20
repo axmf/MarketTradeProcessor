e5cb9c28cd5fcec3c26f7e9735ef466f7ee54302
axmf <axmf55@gmail.com>
2022-11-20 16:13:39 +0100

    Cleanup

--- marketprocessor-processor/src/main/java/com/marketprocessor/MarketProcessorApplication.java
+++ marketprocessor-processor/src/main/java/com/marketprocessor/MarketProcessorApplication.java
@@ -1,18 +1,7 @@
 package com.marketprocessor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.marketprocessor.model.Trade;
-import com.marketprocessor.model.UserVolumeEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.GroupedFlux;
-import reactor.core.publisher.Mono;
-
-import java.time.Duration;
-import java.util.function.Function;
 
 @SpringBootApplication
 public class MarketProcessorApplication {
